c1d2182e39e0566ad2d78ec8031412140502d20c
AartiW-12 <aartiwhaval12@gmail.com>
2025-09-26 12:29:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+const button = document.querySelector("input[type='button']");
+const select = document.getElementById("colorSelect")
+
+button.addEventListener("click", ()=>{
+	let selectedIndex= select.selectedIndex;
+	if(selectedIndex >=0 ){
+		select.remove(selectedIndex)
+	}
+	
+})
